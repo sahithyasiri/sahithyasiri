04686019cd6f262e0dac7230bb2806e30fdee7ee
sahithya <123406355+sahithyasiri@users.noreply.github.com>
2023-09-25 22:29:53 -0500

    Completed Successfully
    Sample Project.

---
+++ SampleExam/BookFlightVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleExam
+//
+//  Created by Chandra on 8/23/23.
+//
+
+import UIKit
+
+class BookFlightVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("Hello")
+    }
+
+
+}
+
